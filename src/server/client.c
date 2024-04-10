89a002bd182c69646c3cceb932fe6a8f321eadbf
Clément <clement2104.boillot@gmail.com>
2024-04-10 18:49:58 +0200

    feat(server): add buffer management

--- src/server/client.c
+++ src/server/client.c
@@ -6,8 +6,10 @@
 */
 
 #include <bits/types.h>
+#include <string.h>
 #include <sys/select.h>
 
+#include "debug.h"
 #include "server.h"
 
 static bool client_wrote(client_t *client)
@@ -16,6 +18,8 @@ static bool client_wrote(client_t *client)
     fd_set fdwrite;
     struct timeval tv = {.tv_sec = 0, .tv_usec = 1000};
 
+    if (client->fd == -1)
+        return false;
     FD_ZERO(&fdread);
     FD_ZERO(&fdwrite);
     FD_SET(client->fd, &fdread);
@@ -25,8 +29,63 @@ static bool client_wrote(client_t *client)
     return true;
 }
 
-void handle_client(UNUSED server_t *serv, client_t *client)
+static void client_disconnect(client_t *client)
 {
+    close(client->fd);
+    client->fd = -1;
+}
+
+static void exec_command(UNUSED server_t *server, client_t *client)
+{
+    char *buffer = client->buffer;
+
+    printf("buffer: `%s`\n", buffer);
+    dprintf(client->fd, "PONG\n");
+}
+
+static void client_process_message(
+    server_t *serv, client_t *client, char *buffer)
+{
+    char *ptr = NULL;
+
+    if (client == NULL || buffer == NULL || buffer[0] == '\0')
+        return;
+    DEBUG("client said: %s", buffer);
+    ptr = strchr(buffer, '\n');
+    if (ptr == NULL) {
+        strcat(client->buffer, buffer);
+        return;
+    }
+    *ptr = '\0';
+    if (buffer[0] != '\0' && *(ptr - 1) == '\r')
+        *(ptr - 1) = '\0';
+    strcat(client->buffer, buffer);
+    DEBUG("buf: %s\n", client->buffer);
+    exec_command(serv, client);
+    client->buffer[0] = '\0';
+    client_process_message(serv, client, ptr + 1);
+}
+
+void handle_client(server_t *serv, client_t *client)
+{
+    char buffer[BUFSIZ + 1];
+    ssize_t size = 0;
+    fd_set fdread;
+    struct timeval tv = {.tv_sec = 0, .tv_usec = 1000};
+
     if (!client_wrote(client))
         return;
+    if (client == NULL)
+        return;
+    FD_ZERO(&fdread);
+    FD_SET(client->fd, &fdread);
+    if (select(client->fd + 1, &fdread, NULL, NULL, &tv) <= 0)
+        return;
+    size = read(client->fd, buffer, BUFSIZ);
+    if (size == 0 || size == -1) {
+        client_disconnect(client);
+        return;
+    }
+    buffer[size] = 0;
+    client_process_message(serv, client, buffer);
 }
--- src/server/server.h
+++ src/server/server.h
@@ -83,7 +83,7 @@ typedef struct {
     struct sockaddr_in addr;
     socklen_t len;
     user_t user;
-    char buffer_cmd[BUFSIZ];
+    char buffer[BUFSIZ];
 } client_t;
 
 typedef struct {
