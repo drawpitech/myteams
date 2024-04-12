323c860e98d7457159115d9810a9ebffbaa48fa0
celian <celian.lombardot@epitech.eu>
2024-04-12 04:02:45 +0200

    fix: remove all pointers in communication struct to be able to write and read them

--- src/ressources_infos.h
+++ src/ressources_infos.h
@@ -7,43 +7,45 @@
 
 #pragma once
 
+#include "utils.h"
 #include <time.h>
+#include <uuid/uuid.h>
 
 typedef struct team_info {
-    char *team_uuid;
-    char *team_name;
-    char *team_description;
+    uuid_t team_uuid;
+    char team_name[MAX_NAME_LENGTH];
+    char team_description[MAX_DESCRIPTION_LENGTH];
 } team_info_t;
 
 typedef struct channel_info {
-    char *channel_uuid;
-    char *channel_name;
-    char *channel_description;
+    uuid_t channel_uuid;
+    char channel_name[MAX_NAME_LENGTH];
+    char channel_description[MAX_DESCRIPTION_LENGTH];
 } channel_info_t;
 
 typedef struct thread_info {
-    char *thread_uuid;
-    char *user_uuid;
+    uuid_t thread_uuid;
+    uuid_t user_uuid;
     time_t thread_timestamp;
-    char *thread_title;
-    char *thread_body;
+    char thread_title[MAX_NAME_LENGTH];
+    char thread_body[MAX_BODY_LENGTH];
 } thread_info_t;
 
 typedef struct reply_info {
-    char *thread_uuid;
-    char *user_uuid;
+    uuid_t thread_uuid;
+    uuid_t user_uuid;
     time_t reply_timestamp;
-    char *reply_body;
+    char reply_body[MAX_BODY_LENGTH];
 } reply_info_t;
 
 typedef struct message_info {
-    char *sender_uuid;
+    uuid_t sender_uuid;
     time_t message_timestamp;
-    char *message_body;
+    char message_body[MAX_BODY_LENGTH];
 } message_info_t;
 
 typedef struct user_info {
-    char *user_uuid;
-    char *user_name;
+    uuid_t user_uuid;
+    char user_name[MAX_NAME_LENGTH];
     int user_status;
 } user_info_t;
