efb18dfa48fbcf568074ca1edef9019c204efb7c
celian <celian.lombardot@epitech.eu>
2024-04-21 22:12:34 +0200

    add: tests on create reply and thread

--- tests/event_test.py
+++ tests/event_test.py
@@ -6,6 +6,8 @@ def serv_usr_created(reply, name):
     assert reply[2] == name
     return uuid
 
+
+
 def serv_team_created(reply, name, user_uuid):
     assert reply[0] == "server_event_team_created"
     uuid = reply[1]
@@ -19,6 +21,22 @@ def serv_chan_created(reply, name, team_uuid):
     uuid = reply[2]
     assert reply[3] == name
     return uuid
+
+def serv_thread_created(reply, name, description, chan_uuid, user_uuid):
+    assert reply[0] == "server_event_thread_created"
+    assert reply[1] == chan_uuid
+    uuid = reply[2]
+    assert reply[3] == user_uuid
+    assert reply[4] == name
+    assert reply[5] == description
+    return uuid
+
+def serv_reply_created(reply, thread_uuid, user, body):
+    assert reply[0] == "server_event_reply_created"
+    assert reply[1] == thread_uuid
+    assert reply[2] == user
+    assert reply[3] == body
+
 
 def serv_usr_sub(reply, user_uuid, team_uuid):
     assert reply[0] == "server_event_user_subscribed"
@@ -45,6 +63,24 @@ def cli_print_chan_created(reply, uuid, name, description):
     assert reply[0] == "client_print_channel_created"
     check_uuid_name_desc(reply, uuid, name, description)
 
+def cli_print_thread_created(reply, name, description, thread_uuid, user_uuid):
+    assert reply[0] == "client_print_thread_created"
+    assert reply[1] == thread_uuid
+    assert reply[2] == user_uuid
+    timestamp = reply[3]
+    assert reply[4] == name
+    assert reply[5] == description
+    return timestamp
+
+def cli_print_reply_created(reply, thread, user, body):
+    assert reply[0] == "client_print_reply_created"
+    assert reply[1] == thread
+    assert reply[2] == user
+    timestamp = reply[3]
+    assert reply[4] == body
+    return timestamp
+
+
 def cli_event_team_created(reply, uuid, name, description):
     assert reply[0] == "client_event_team_created"
     check_uuid_name_desc(reply, uuid, name, description)
@@ -53,6 +89,20 @@ def cli_event_chan_created(reply, uuid, name, description):
     assert reply[0] == "client_event_channel_created"
     check_uuid_name_desc(reply, uuid, name, description)
 
+def cli_event_thread_created(reply, name, description, thread_uuid, user_uuid, timestamp):
+    assert reply[0] == "client_event_thread_created"
+    assert reply[1] == thread_uuid
+    assert reply[2] == user_uuid
+    assert reply[3] == timestamp
+    assert reply[4] == name
+    assert reply[5] == description
+
+def cli_event_reply_created(reply, team, thread, user, body):
+    assert reply[0] == "client_event_thread_reply_received"
+    assert reply[1] == team
+    assert reply[2] == thread
+    assert reply[3] == user
+    assert reply[4] == body
 
 
 
@@ -64,7 +114,20 @@ def cli_print_chans(reply, uuid, name, description):
     assert reply[0] == "client_team_print_channels"
     check_uuid_name_desc(reply, uuid, name, description)
 
+def cli_print_threads(reply, name, description, thread_uuid, user_uuid, timestamp):
+    assert reply[0] == "client_channel_print_threads"
+    assert reply[1] == thread_uuid
+    assert reply[2] == user_uuid
+    assert reply[3] == timestamp
+    assert reply[4] == name
+    assert reply[5] == description
 
+def cli_print_replies(reply, thread, user, timestamp, body):
+    assert reply[0] == "client_thread_print_replies"
+    assert reply[1] == thread
+    assert reply[2] == user
+    assert reply[3] == timestamp
+    assert reply[4] == body
 
 def cli_print_team(reply, uuid, name, description):
     assert reply[0] == "client_print_team"
@@ -74,6 +137,13 @@ def cli_print_chan(reply, uuid, name, description):
     assert reply[0] == "client_print_channel"
     check_uuid_name_desc(reply, uuid, name, description)
 
+def cli_print_thread(reply, name, description, thread_uuid, user_uuid, timestamp):
+    assert reply[0] == "client_print_thread"
+    assert reply[1] == thread_uuid
+    assert reply[2] == user_uuid
+    assert reply[3] == timestamp
+    assert reply[4] == name
+    assert reply[5] == description
 
 
 def cli_prin_sub(reply, user_uuid, team_uuid):
--- tests/test_create.py
+++ tests/test_create.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import event_test
 from event_test import *
 
-def check_team_creation(team: Team, uuid1):
+def team_creation(team: Team, uuid1):
     team_name = "TEAM"
     team_description = "descrpition aaaaaaa"
     serv, cli = team.run([f'/create "{team_name}" "{team_description}"'])
@@ -11,20 +11,13 @@ def check_team_creation(team: Team, uuid1):
     cli_print_team_created(cli[0], team_uuid, team_name, team_description)
     cli_event_team_created(cli[1], team_uuid, team_name, team_description)
 
-
-    serv, cli = team.run([f'/list'], 0.2)
-    cli_print_teams(cli[0], team_uuid, team_name, team_description)
-
-
-    serv, cli = team.run([f'/use "{team_uuid}"'], 0.2)
     serv, cli = team.run([f'/subscribe "{team_uuid}"'], 0.2)
     serv_usr_sub(serv[0], uuid1, team_uuid)
     cli_prin_sub(cli[0], uuid1, team_uuid)
-    serv, cli = team.run([f'/info'], 0.2)
-    cli_print_team(cli[0], team_uuid, team_name, team_description)
-    return team_uuid
 
-def check_chan_creation(team: Team, uuid1, team_uuid):
+    return team_uuid, team_name, team_description
+
+def chan_creation(team: Team, uuid1, team_uuid):
     chan_name = "CHAN"
     chan_description = "never gonna give you up"
     serv, cli = team.run([f'/create "{chan_name}" "{chan_description}"'])
@@ -32,12 +25,27 @@ def check_chan_creation(team: Team, uuid1, team_uuid):
     cli_print_chan_created(cli[0], chan_uuid, chan_name, chan_description)
     cli_event_chan_created(cli[1], chan_uuid, chan_name, chan_description)
 
-    serv, cli = team.run([f'/list'], 0.2)
-    cli_print_chans(cli[0], chan_uuid, chan_name, chan_description)
-    serv, cli = team.run([f'/use "{team_uuid}" "{chan_uuid}"'], 0.2)
-    serv, cli = team.run([f'/info'], 0.2)
-    cli_print_chan(cli[0], chan_uuid, chan_name, chan_description)
-    return chan_uuid
+    return chan_uuid, chan_name, chan_description
+
+def thread_creation(team: Team, uuid1, team_uuid, chan_uuid):
+    thread_name = "THREAD"
+    thread_description = "thread body"
+    serv, cli = team.run([f'/create "{thread_name}" "{thread_description}"'])
+    thread_uuid = serv_thread_created(serv[0], thread_name, thread_description, chan_uuid, uuid1)
+    timestamp = cli_print_thread_created(cli[0], thread_name, thread_description, thread_uuid, uuid1)
+    cli_event_thread_created(cli[1], thread_name, thread_description, thread_uuid, uuid1, timestamp)
+
+    return thread_uuid, timestamp, thread_name, thread_description
+
+def reply_creation(team: Team, user, team_uuid, chan, thread):
+    body = "reply"
+    serv, cli = team.run([f'/create "{body}"'])
+    serv_reply_created(serv[0], thread, user, body)
+    timestamp = cli_print_reply_created(cli[0], thread, user, body)
+    cli_event_reply_created(cli[1], team_uuid, thread, user, body)
+    return timestamp
+
+
 
 
 def test_create_team():
@@ -48,23 +56,68 @@ def test_create_team():
     serv, cli = team.run([f'/login "{name}"\n',],0.2)
     uuid1 = serv_usr_created(serv[0], name)
 
-    check_team_creation(team, uuid1)
+    team_uuid, team_name, team_description = team_creation(team, uuid1)
+    serv, cli = team.run([f'/list'], 0.2)
+    cli_print_teams(cli[0], team_uuid, team_name, team_description)
+    serv, cli = team.run([f'/use "{team_uuid}"'], 0.2)
+    serv, cli = team.run([f'/info'], 0.2)
+    cli_print_team(cli[0], team_uuid, team_name, team_description)
     return
-
 
 
 def test_create_chan():
     team = Team()
     name = "toto"
-    serv, cli = team.run([f'/create\n'],0.2)
-    assert cli[0][0] == 'client_error_unauthorized'
     serv, cli = team.run([f'/login "{name}"\n',],0.2)
     uuid1 = serv_usr_created(serv[0], name)
 
-    team_uuid = check_team_creation(team, uuid1)
-    check_chan_creation(team, uuid1, team_uuid)
+    team_uuid, team_name, team_description = team_creation(team, uuid1)
+    serv, cli = team.run([f'/use "{team_uuid}"'], 0.2)
+    chan_uuid, chan_name, chan_description = chan_creation(team, uuid1, team_uuid)
+    serv, cli = team.run([f'/list'], 0.2)
+    cli_print_chans(cli[0], chan_uuid, chan_name, chan_description)
+    serv, cli = team.run([f'/use "{team_uuid}" "{chan_uuid}"'], 0.2)
+    serv, cli = team.run([f'/info'], 0.2)
+    cli_print_chan(cli[0], chan_uuid, chan_name, chan_description)
+    return
+
+def test_create_thread():
+    team = Team()
+    name = "toto"
+    serv, cli = team.run([f'/login "{name}"\n',],0.2)
+    uuid1 = serv_usr_created(serv[0], name)
+
+    team_uuid, team_name, team_description = team_creation(team, uuid1)
+    serv, cli = team.run([f'/use "{team_uuid}"'], 0.2)
+    chan_uuid, chan_name, chan_description = chan_creation(team, uuid1, team_uuid)
+    serv, cli = team.run([f'/use "{team_uuid}" "{chan_uuid}"'], 0.2)
+    thread_uuid, timestamp, thread_name, thread_description = thread_creation(team, uuid1, team_uuid, chan_uuid)
+    serv, cli = team.run([f'/list'], 0.2)
+    cli_print_threads(cli[0], thread_name, thread_description, thread_uuid, uuid1, timestamp)
+    serv, cli = team.run([f'/use "{team_uuid}" "{chan_uuid}" "{thread_uuid}"'], 0.2)
+    serv, cli = team.run([f'/info'], 0.2)
+    cli_print_thread(cli[0], thread_name, thread_description, thread_uuid, uuid1, timestamp)
+    return
+
+def test_create_reply():
+    team = Team()
+    name = "toto"
+    serv, cli = team.run([f'/login "{name}"\n',],0.2)
+    uuid1 = serv_usr_created(serv[0], name)
+
+    team_uuid, team_name, team_description = team_creation(team, uuid1)
+    serv, cli = team.run([f'/use "{team_uuid}"'], 0.2)
+    chan_uuid, chan_name, chan_description = chan_creation(team, uuid1, team_uuid)
+    serv, cli = team.run([f'/use "{team_uuid}" "{chan_uuid}"'], 0.2)
+    thread_uuid, timestamp, thread_name, thread_description = thread_creation(team, uuid1, team_uuid, chan_uuid)
+    serv, cli = team.run([f'/use "{team_uuid}" "{chan_uuid}" "{thread_uuid}"'], 0.2)
+    reply_creation(team, uuid1, team_uuid, chan_uuid, thread_uuid)
+
+    serv, cli = team.run([f'/list'], 0.2)
     return
 
 if __name__ == "__main__":
     # test_create_team()
-    test_create_chan()
+    # test_create_chan()
+    # test_create_thread()
+    test_create_reply()
